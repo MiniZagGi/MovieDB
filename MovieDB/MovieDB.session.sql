14232d5ab7e63f56fa0194ba4805d6b897ff5688
MiniZagGi <33091100+MiniZagGi@users.noreply.github.com>
2024-05-22 15:59:59 +0200

    Added from Homelab repo

---
+++ MovieDB/MovieDB.session.sql
@@ -0,0 +1,11 @@
+INSERT INTO Movie (DirectorID, Title, ReleaseYear)
+VALUES (1, 'The Shawshank Redemption', 1994),
+       (2, 'The Godfather', 1972),
+       (3, 'The Dark Knight', 2008),
+       (4, 'The Lord of the Rings: The Return of the King', 2003),
+       (5, 'Pulp Fiction', 1994),
+       (6, 'Forrest Gump', 1994),
+       (7, 'The Matrix', 1999),
+       (8, 'The Lord of the Rings: The Fellowship of the Ring', 2001),
+       (9, 'The Lord of the Rings: The Two Towers', 2002),
+       (10, 'The Godfather: Part II', 1974);
